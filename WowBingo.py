5c2e1b60d2f9b4a18536feeace593a185fb0aed2
Woody Thompson <relambrien@gmail.com>
2017-06-25 17:42:26 -0700

    Add free-space functionality
    Adds a new file "free.txt" containing a list of "free space" options. The
    generator always chooses a random one of these options to fill the middle
    square on the board.
    
    Currently "free.txt" contains only one option, a single empty space.

--- WowBingo.py
+++ WowBingo.py
@@ -5,9 +5,12 @@ from BingoBoard import BingoBoard
 
 class WowBingo:
 
-    def __init__(self, grid_size = 5, file_name = 'cards.txt'):
+    FREE_SPACE = " (FREE SPACE)"
+
+    def __init__(self, grid_size = 5, file_name = 'cards.txt', free_name = 'free.txt'):
         self.grid_size = grid_size
         self.bingo_options = self._read_file(file_name)
+        self.free_options = self._read_file(free_name)
 
     def _read_file(self, file_name):
         bingo_file = open(file_name, 'rt', encoding="utf8")
@@ -24,16 +27,22 @@ class WowBingo:
 
     def generate_board_layout(self):
         randRange = range(0,len(self.bingo_options))
-        option_pool = random.sample(randRange, self.grid_size * self.grid_size)
+        option_pool = random.sample(randRange, (self.grid_size * self.grid_size) - 1)
+        free_space = self.free_options[random.randint(0, len(self.free_options) - 1)][0] + self.FREE_SPACE
 
         board = []
         row = []
 
+        squares_appended = 0
         for option in option_pool:
+            # Bingo boards are always odd-sized, so this gives us the median square
+            if squares_appended == self.grid_size * self.grid_size // 2:
+                row.append([free_space])
             if len(row) == self.grid_size:
                 board.append(row)
                 row = []
             row.append(self.bingo_options[option])
+            squares_appended = squares_appended+1
         board.append(row)
 
         return board
